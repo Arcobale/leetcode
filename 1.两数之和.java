b51f0b337794565f154f41d9f11afdfd2457a217
Arcobale <1144209486@qq.com>
2022-08-03 17:30:54 +0800

    哈希

--- 1.两数之和.java
+++ 1.两数之和.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+
 /*
  * @lc app=leetcode.cn id=1 lang=java
  *
@@ -7,13 +9,12 @@
 // @lc code=start
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int len = nums.length;
-        for (int i = 0; i < len - 1; i++) {
-            for (int j = i + 1; j < len; j++) {
-                if (nums[i] + nums[j] == target) {
-                    return new int[] { i, j };
-                }
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; ++i) {
+            if (map.containsKey(target - nums[i])) {
+                return new int[] { map.get(target - nums[i]), i };
             }
+            map.put(nums[i], i);
         }
         return new int[0];
     }
