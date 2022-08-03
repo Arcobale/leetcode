178a7d67f407418f24e9e4d23fbd8fced4ea7519
Arcobale <1144209486@qq.com>
2022-08-03 16:27:18 +0800

    暴力

---
+++ 1.两数之和.java
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode.cn id=1 lang=java
+ *
+ * [1] 两数之和
+ */
+
+// @lc code=start
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int len = nums.length;
+        for (int i = 0; i < len - 1; i++) {
+            for (int j = i + 1; j < len; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[] { i, j };
+                }
+            }
+        }
+        return new int[0];
+    }
+}
+// @lc code=end
