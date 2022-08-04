bf2a37ed8823875585cdefe4041abf697ce6b1c4
Arcobale <1144209486@qq.com>
2022-08-04 09:18:38 +0800

    栈-优化

--- 20.有效的括号.java
+++ 20.有效的括号.java
@@ -7,34 +7,16 @@
 // @lc code=start
 class Solution {
     public boolean isValid(String s) {
-        if (s.length() == 0) {
-            return true;
-        }
-
         Stack<Character> stack = new Stack<>();
         for (char c : s.toCharArray()) {
-            if (c == '(' || c == '[' || c == '{') {
-                stack.push(c);
-            } else {
-                if (stack.isEmpty()) {
-                    return false;
-                } else {
-                    char temp = stack.pop();
-                    if (c == ')') {
-                        if (temp != '(') {
-                            return false;
-                        }
-                    } else if (c == ']') {
-                        if (temp != '[') {
-                            return false;
-                        }
-                    } else if (c == '}') {
-                        if (temp != '{') {
-                            return false;
-                        }
-                    }
-                }
-            }
+            if (c == '(')
+                stack.push(')');
+            else if (c == '[')
+                stack.push(']');
+            else if (c == '{')
+                stack.push('}');
+            else if (stack.isEmpty() || c != stack.pop())
+                return false;
         }
         return stack.isEmpty();
     }
