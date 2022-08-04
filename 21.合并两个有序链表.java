261bb4f592100c1761aec7992dee3e2bf5107ba5
Arcobale <1144209486@qq.com>
2022-08-04 09:49:27 +0800

    递归

--- 21.合并两个有序链表.java
+++ 21.合并两个有序链表.java
@@ -17,20 +17,17 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        ListNode result = new ListNode();
-        ListNode cur = result;
-        while (list1 != null && list2 != null) {
-            if (list1.val <= list2.val) {
-                cur.next = list1;
-                list1 = list1.next;
-            } else {
-                cur.next = list2;
-                list2 = list2.next;
-            }
-            cur = cur.next;
+        if (list1 == null) {
+            return list2;
+        } else if (list2 == null) {
+            return list1;
+        } else if (list1.val <= list2.val) {
+            list1.next = mergeTwoLists(list1.next, list2);
+            return list1;
+        } else {
+            list2.next = mergeTwoLists(list1, list2.next);
+            return list2;
         }
-        cur.next = list1 == null ? list2 : list1;
-        return result.next;
     }
 }
 // @lc code=end
