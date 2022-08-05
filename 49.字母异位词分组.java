f0572de47885eea1acfd5072fcbfbd5282b524c7
Arcobale <1144209486@qq.com>
2022-08-05 14:25:20 +0800

    排序

---
+++ 49.字母异位词分组.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/*
+ * @lc app=leetcode.cn id=49 lang=java
+ *
+ * [49] 字母异位词分组
+ */
+
+// @lc code=start
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<String, ArrayList<String>> map = new HashMap();
+        for (String s : strs) {
+            char[] ch = s.toCharArray();
+            Arrays.sort(ch);
+            String key = new String(ch);
+            if (!map.containsKey(key)) {
+                map.put(key, new ArrayList<>());
+            }
+            map.get(key).add(s);
+        }
+        return new ArrayList<>(map.values());
+    }
+}
+// @lc code=end
