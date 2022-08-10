5515b0dad51ba61ea99d1443bf07c37dc180fbc6
Arcobale <1144209486@qq.com>
2022-08-10 18:27:15 +0800

    哈希-set

---
+++ 202.快乐数.java
@@ -0,0 +1,29 @@
+import java.util.HashSet;
+
+/*
+ * @lc app=leetcode.cn id=202 lang=java
+ *
+ * [202] 快乐数
+ */
+
+// @lc code=start
+class Solution {
+    public boolean isHappy(int n) {
+        Set<Integer> set = new HashSet<>();
+        while (n != 1 && !set.contains(n)) {
+            set.add(n);
+            n = getNextNumber(n);
+        }
+        return n == 1;
+    }
+
+    private int getNextNumber(int n) {
+        int sum = 0;
+        while (n > 0) {
+            sum += (n % 10) * (n % 10);
+            n /= 10;
+        }
+        return sum;
+    }
+}
+// @lc code=end
