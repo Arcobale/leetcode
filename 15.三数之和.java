077abc4803db9dd5c62edc04c455f054449d7a19
Arcobale <1144209486@qq.com>
2022-08-11 14:34:15 +0800

    双指针

---
+++ 15.三数之和.java
@@ -0,0 +1,49 @@
+import java.util.Arrays;
+
+/*
+ * @lc app=leetcode.cn id=15 lang=java
+ *
+ * [15] 三数之和
+ */
+
+// @lc code=start
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums);
+
+        for (int i = 0; i < nums.length; i++) {
+            // 排序之后如果第一个元素已经大于零，那么无论如何组合都不可能凑成三元组，直接返回结果就可以了
+            if (nums[i] > 0) {
+                return result;
+            }
+            // 去重a
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            int left = i + 1;
+            int right = nums.length - 1;
+            while (left < right) {
+                if (nums[i] + nums[left] + nums[right] > 0) {
+                    right--;
+                } else if (nums[i] + nums[left] + nums[right] < 0) {
+                    left++;
+                } else {
+                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    // 去重逻辑应该放在找到一个三元组之后，对b 和 c去重
+                    while (left < right && nums[right] == nums[right - 1]) {
+                        right--;
+                    }
+                    while (left < right && nums[left] == nums[left + 1]) {
+                        left++;
+                    }
+                    // 找到答案时，双指针同时收缩
+                    right--;
+                    left++;
+                }
+            }
+        }
+        return result;
+    }
+}
+// @lc code=end
