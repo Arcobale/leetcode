9aa18c61466ab561eb3ca854a351c8579869c2d9
Arcobale <1144209486@qq.com>
2022-08-16 20:10:07 +0800

    二叉树-层序遍历（迭代）

---
+++ 637.二叉树的层平均值.java
@@ -0,0 +1,51 @@
+/*
+ * @lc app=leetcode.cn id=637 lang=java
+ *
+ * [637] 二叉树的层平均值
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
+ * }
+ */
+class Solution {
+    public List<Double> averageOfLevels(TreeNode root) {
+        List<Double> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+        Queue<TreeNode> que = new LinkedList<TreeNode>();
+        que.offer(root);
+
+        while (!que.isEmpty()) {
+            int len = que.size();
+            double sum = 0.0;
+
+            for (int i = 0; i < len; i++) {
+                TreeNode temp = que.poll();
+                sum += temp.val;
+                if (temp.left != null) {
+                    que.offer(temp.left);
+                }
+                if (temp.right != null) {
+                    que.offer(temp.right);
+                }
+            }
+            result.add(sum / len);
+        }
+        return result;
+    }
+}
+// @lc code=end
