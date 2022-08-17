ce95f7a68c847173fce6ea77ddd5f60e07127173
Arcobale <1144209486@qq.com>
2022-08-17 15:32:23 +0800

    二叉树-DFS（递归）

---
+++ 222.完全二叉树的节点个数.java
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode.cn id=222 lang=java
+ *
+ * [222] 完全二叉树的节点个数
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
+ * }
+ */
+class Solution {
+    public int countNodes(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int leftSum = countNodes(root.left); // 左
+        int rightSum = countNodes(root.right); // 右
+        return leftSum + rightSum + 1; // 中
+    }
+}
+// @lc code=end
