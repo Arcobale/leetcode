3e3e1dae99ce18badf0112206c2e22163096e777
Arcobale <1144209486@qq.com>
2022-08-17 20:41:02 +0800

    二叉树-递归

---
+++ 404.左叶子之和.java
@@ -0,0 +1,37 @@
+/*
+ * @lc app=leetcode.cn id=404 lang=java
+ *
+ * [404] 左叶子之和
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
+ * }
+ */
+class Solution {
+    public int sumOfLeftLeaves(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int leftValue = sumOfLeftLeaves(root.left); // 左
+        int rightValue = sumOfLeftLeaves(root.right); // 右
+        int midValue = 0;
+        if (root.left != null && root.left.left == null && root.left.right == null) {
+            midValue += root.left.val;
+        }
+        return leftValue + rightValue + midValue; // 中
+    }
+}
+// @lc code=end
