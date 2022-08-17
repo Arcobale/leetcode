072c9818a36172ccec6a53a7e786049732c9a421
Arcobale <1144209486@qq.com>
2022-08-17 12:37:10 +0800

    二叉树-DFS（递归）

---
+++ 226.翻转二叉树.java
@@ -0,0 +1,39 @@
+import javax.swing.tree.TreeNode;
+
+/*
+ * @lc app=leetcode.cn id=226 lang=java
+ *
+ * [226] 翻转二叉树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
+ * }
+ */
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return root;
+        }
+        // 前后序遍历都可以
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+        invertTree(root.left);
+        invertTree(root.right);
+        return root;
+    }
+}
+// @lc code=end
