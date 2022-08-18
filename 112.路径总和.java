026659a92b51118fd4cbbdf991dd845a205d0706
Arcobale <1144209486@qq.com>
2022-08-18 13:34:54 +0800

    二叉树-迭代

--- 112.路径总和.java
+++ 112.路径总和.java
@@ -1,3 +1,7 @@
+import java.util.Stack;
+
+import javax.swing.tree.TreeNode;
+
 /*
  * @lc app=leetcode.cn id=112 lang=java
  *
@@ -25,10 +29,32 @@ class Solution {
         if (root == null) {
             return false;
         }
-        if (root.left == null && root.right == null && targetSum == root.val) {
-            return true;
+        /*
+         * if (root.left == null && root.right == null && targetSum == root.val) {
+         * return true;
+         * }
+         * return hasPathSum(root.left, targetSum - root.val) || hasPathSum(root.right,
+         * targetSum - root.val);
+         */
+        Stack<Object> stack = new Stack<>();
+        stack.push(root);
+        stack.push(root.val);
+        while (!stack.isEmpty()) {
+            int val = (int) stack.pop();
+            TreeNode node = (TreeNode) stack.pop();
+            if (node.left == null && node.right == null && val == targetSum) {
+                return true;
+            }
+            if (node.right != null) {
+                stack.push(node.right);
+                stack.push(val + node.right.val);
+            }
+            if (node.left != null) {
+                stack.push(node.left);
+                stack.push(val + node.left.val);
+            }
         }
-        return hasPathSum(root.left, targetSum - root.val) || hasPathSum(root.right, targetSum - root.val);
+        return false;
     }
 }
 // @lc code=end
