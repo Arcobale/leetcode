7d261f5406786cf13e1f40e3aa63c795e2a790ab
Arcobale <1144209486@qq.com>
2022-08-19 21:28:54 +0800

    二叉树-二叉搜索树-递归

---
+++ 108.将有序数组转换为二叉搜索树.java
@@ -0,0 +1,47 @@
+import javax.swing.tree.TreeNode;
+
+import apple.laf.JRSUIUtils.Tree;
+
+/*
+ * @lc app=leetcode.cn id=108 lang=java
+ *
+ * [108] 将有序数组转换为二叉搜索树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
+ * }
+ */
+class Solution {
+    public TreeNode sortedArrayToBST(int[] nums) {
+        return traversal(nums, 0, nums.length);
+    }
+
+    private TreeNode traversal(int[] nums, int left, int right) {
+        if (left >= right) {
+            return null;
+        }
+        if (right - left == 1) {
+            return new TreeNode(nums[left]);
+        }
+        int mid = left + (right - left) / 2;
+        TreeNode node = new TreeNode(nums[mid]);
+        node.left = traversal(nums, left, mid);
+        node.right = traversal(nums, mid + 1, right);
+
+        return node;
+    }
+}
+// @lc code=end
