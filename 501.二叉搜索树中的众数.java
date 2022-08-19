eaec2f17429797e1131b7d660b2dbb6104a7972e
Arcobale <1144209486@qq.com>
2022-08-19 14:47:28 +0800

    二叉树-二叉搜索树-递归

--- 501.二叉搜索树中的众数.java
+++ 501.二叉搜索树中的众数.java
@@ -27,42 +27,44 @@ import java.util.PriorityQueue;
  * }
  */
 class Solution {
-    public Map<Integer, Integer> map = new HashMap<>();
+    List<Integer> result = new ArrayList<>();
+    int maxCount = 0;
+    int count = 0;
+    TreeNode pre = null;
 
     public int[] findMode(TreeNode root) {
-        List<Integer> result = new ArrayList<>();
         if (root == null) {
             return result.stream().mapToInt(Integer::intValue).toArray();
         }
         traversal(root);
 
-        Set<Map.Entry<Integer, Integer>> entries = map.entrySet();
-        PriorityQueue<Map.Entry<Integer, Integer>> que = new PriorityQueue<>((o1, o2) -> o2.getValue() - o1.getValue());
-        for (Map.Entry<Integer, Integer> entry : entries) {
-            que.offer(entry);
-        }
-
-        int max = que.peek().getValue();
-        int len = que.size();
-        result.add(que.poll().getKey());
-
-        for (int i = 1; i < len; i++) {
-            if (que.peek().getValue() == max) {
-                result.add(que.poll().getKey());
-            } else {
-                break;
-            }
-        }
         return result.stream().mapToInt(Integer::intValue).toArray();
     }
 
-    private void traversal(TreeNode node) {
-        if (node == null) {
+    private void traversal(TreeNode cur) {
+        if (cur == null) {
             return;
         }
-        map.put(node.val, map.getOrDefault(node.val, 0) + 1);
-        traversal(node.left);
-        traversal(node.right);
+        traversal(cur.left);
+
+        if (pre == null) {
+            count = 1;
+        } else if (pre.val == cur.val) {
+            count++;
+        } else {
+            count = 1;
+        }
+        pre = cur;
+
+        if (count == maxCount) {
+            result.add(cur.val);
+        } else if (count > maxCount) {
+            maxCount = count;
+            result.clear();
+            result.add(cur.val);
+        }
+
+        traversal(cur.right);
     }
 }
 // @lc code=end
