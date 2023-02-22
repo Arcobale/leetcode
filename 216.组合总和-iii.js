f11e686d15add4af8f49eeaa61d1bee3d4e51281
Arcobale <1144209486@qq.com>
2023-02-22 23:15:21 +0800

    回溯

---
+++ 216.组合总和-iii.js
@@ -0,0 +1,39 @@
+/*
+ * @lc app=leetcode.cn id=216 lang=javascript
+ *
+ * [216] 组合总和 III
+ */
+
+// @lc code=start
+/**
+ * @param {number} k
+ * @param {number} n
+ * @return {number[][]}
+ */
+let result = [];
+let path = [];
+var combinationSum3 = function(k, n) {
+    result = [];
+    backtracking(k, n, 0, 1);
+    return result;
+};
+function backtracking(k, n, sum, startIndex) {
+    if (path.length == k && sum == n) {
+        result.push([...path]);
+        return;
+    }
+    for (let i = startIndex; i <= 9 - (k - path.length) + 1; i++) {
+        path.push(i);
+        sum += i;
+        if (sum > n) {
+            path.pop();
+            sum -= i;
+            return;
+        }
+        backtracking(k, n, sum, i + 1);
+        path.pop();
+        sum -= i;
+    }
+}
+// @lc code=end
+
