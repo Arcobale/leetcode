fbc93a8364e3ac17b833b0bf3ef0ae784857f202
Arcobale <1144209486@qq.com>
2023-02-24 12:44:03 +0800

    贪心

---
+++ 376.摆动序列.js
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode.cn id=376 lang=javascript
+ *
+ * [376] 摆动序列
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var wiggleMaxLength = function(nums) {
+    let result = 1;
+    let pre = 0;
+    let cur = 0;
+    for (let i = 0; i < nums.length - 1; i++) {
+        let cur = nums[i + 1] - nums[i];
+        if (pre >= 0 && cur < 0 || pre <= 0 && cur > 0) {
+            result++;
+            pre = cur;
+        }
+    }
+
+    return result;
+};
+// @lc code=end
+
