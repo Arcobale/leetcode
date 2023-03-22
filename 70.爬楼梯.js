1299a472212d2f9886d0c2dd3637eadd46659f1d
Arcobale <1144209486@qq.com>
2023-03-22 23:42:17 +0800

    动态规划-完全背包-排列数

--- 70.爬楼梯.js
+++ 70.爬楼梯.js
@@ -10,11 +10,21 @@
  * @return {number}
  */
 var climbStairs = function(n) {
-    let dp = new Array(n + 1);
+    let dp = new Array(n + 1).fill(0);
+/*  //斐波那契式
     dp[1] = 1;
     dp[2] = 2;
     for (let i = 3; i <= n; i++) {
         dp[i] = dp[i - 1] + dp[i - 2];
+    }
+     */
+    //背包问题
+    dp[0] = 1;
+    let step = [1, 2];
+    for (let i = 1; i <= n; i++){
+        for (let j = 0; j < step.length; j++) {
+            if(i >= step[j]) dp[i] += dp[i - step[j]];
+        }
     }
     return dp[n];
 };
