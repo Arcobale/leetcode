8ad40952ab7771cf2da7067ce1fb3cfb90d2986f
Arcobale <1144209486@qq.com>
2023-03-23 13:18:40 +0800

    动态规划

--- 122.买卖股票的最佳时机-ii.js
+++ 122.买卖股票的最佳时机-ii.js
@@ -10,6 +10,7 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
+/*  //贪心
     let result = 0;
     for (let i = 1; i < prices.length; i++) {
         if (prices[i] - prices[i - 1] > 0) {
@@ -17,6 +18,15 @@ var maxProfit = function(prices) {
         }
     }
     return result;
+ */
+    let dp = new Array(prices.length).fill([0, 0]);
+    dp[0][0] = -prices[0];
+    dp[0][1] = 0;
+    for (let i = 1; i < prices.length; i++) {
+        dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] - prices[i]);
+        dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] + prices[i]);
+    }
+    return dp[prices.length - 1][1];
 };
 // @lc code=end
 
