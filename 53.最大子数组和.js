b632231f6d1293dadb039fb011703fbd460d2d21
Arcobale <1144209486@qq.com>
2023-03-23 16:58:17 +0800

    动态规划-序列

--- 53.最大子数组和.js
+++ 53.最大子数组和.js
@@ -10,12 +10,22 @@
  * @return {number}
  */
 var maxSubArray = function(nums) {
+/*     //贪心
     let result = -Infinity;
     let count = 0;  //保存连续和
     for (let num of nums) {
         count += num;
         if (count > result) result = count;
         if (count < 0) count = 0;
+    }
+    return result;
+ */
+    let dp = new Array(nums.length).fill(-Infinity);
+    dp[0] = nums[0];
+    let result = 0;
+    for (let i = 1; i < nums.length; i++) {
+        dp[i] = Math.max(dp[i - 1] + nums[i], nums[i]);
+        result = Math.max(result, dp[i]);
     }
     return result;
 };
